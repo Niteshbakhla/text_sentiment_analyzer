dec2e9e0e54045020aeb223c03ea7c8d112330f1
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-02 12:20:44 +0530

    added  tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,40 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        gradient: "gradient 8s linear infinite",
-        jump: 'jump 0.3s ease',
-        fadeIn: 'fadeIn 0.5s ease-out',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0', transform: 'translateY(-20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+export default withMT(
+  {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        animation: {
+          gradient: "gradient 8s linear infinite",
+          jump: 'jump 0.3s ease',
+          fadeIn: 'fadeIn 0.5s ease-out',
         },
-        jump: {
-          '0%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        gradient: {
-          '0%': {
-            backgroundPosition: '0% 0%',
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0', transform: 'translateY(-20px)' },
+            '100%': { opacity: '1', transform: 'translateY(0)' },
           },
-          '100%': {
-            backgroundPosition: '100% 100%',
+          jump: {
+            '0%': { transform: 'translateY(0)' },
+            '50%': { transform: 'translateY(-10px)' },
+            '100%': { transform: 'translateY(0)' },
+          },
+          gradient: {
+            '0%': {
+              backgroundPosition: '0% 0%',
+            },
+            '100%': {
+              backgroundPosition: '100% 100%',
+            },
           },
         },
       },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
+)
