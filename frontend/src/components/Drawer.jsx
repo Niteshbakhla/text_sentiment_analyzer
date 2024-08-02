f8f502464d44c93d3cdbcd720fe1b124606f1aa1
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-02 15:28:18 +0530

    added a new component drawer

---
+++ frontend/src/components/Drawer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+            Drawer,
+            Button,
+            Typography,
+            IconButton,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+
+export function DrawerPlacement({ name }) {
+
+            const [openLeft, setOpenLeft] = React.useState(false);
+            const openDrawerLeft = () => setOpenLeft(true);
+            const closeDrawerLeft = () => setOpenLeft(false);
+
+
+
+
+            return (
+                        <React.Fragment>
+                                    <div className="flex flex-wrap gap-4 fixed left-2 top-4 ">
+                                                <Button onClick={openDrawerLeft} className="rounded-full text-center text-2xl">{name.charAt(0)}</Button>
+                                    </div>
+                                    <Drawer
+                                                placement="left"
+                                                open={openLeft}
+                                                onClose={closeDrawerLeft}
+                                                className="p-4"
+                                    >
+                                                <div className="mb-6 flex items-center justify-between">
+                                                            <Typography variant="h5" color="blue-gray">
+                                                                        {name.charAt(0).toUpperCase() + name.slice(1)}
+                                                            </Typography>
+                                                            <IconButton
+                                                                        variant="text"
+                                                                        color="blue-gray"
+                                                                        onClick={closeDrawerLeft}
+                                                            >
+                                                                        <svg
+                                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                                    fill="none"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    strokeWidth={2}
+                                                                                    stroke="currentColor"
+                                                                                    className="h-5 w-5"
+                                                                        >
+                                                                                    <path
+                                                                                                strokeLinecap="round"
+                                                                                                strokeLinejoin="round"
+                                                                                                d="M6 18L18 6M6 6l12 12"
+                                                                                    />
+                                                                        </svg>
+                                                            </IconButton>
+                                                </div>
+
+                                                <div className="flex gap-2">
+                                                            <Link to="/history">
+                                                                        <Button size="sm" variant="outlined">
+                                                                                    History
+                                                                        </Button>
+                                                            </Link>
+                                                </div>
+                                    </Drawer>
+                        </React.Fragment>
+            );
+}
