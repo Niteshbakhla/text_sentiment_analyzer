931c4896f259f0dd38838996e028e0b5d32ec4db
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-09 00:44:06 +0530

    added a useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TextInput from './components/TextInput'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route, useNavigate, } from "react-router-dom"
 import Welcome from './components/Welcome'
 import History from './components/ShowHistory'
 
 function App() {
 
   const name = localStorage.getItem("name")
-
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (name) {
+      navigate("/write_text")
+    }
+  }, [])
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Welcome />} />
-          <Route path='/write_text' element={name ? <TextInput /> : "fill your name"} />
-          <Route path='/history' element={<History />} />
-        </Routes>
-      </Router>
+
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/write_text' element={name ? <TextInput /> : "fill your name"} />
+        <Route path='/history' element={<History />} />
+      </Routes>
+
     </>
   )
 }
