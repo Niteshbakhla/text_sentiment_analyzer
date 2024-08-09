afe0fe6d725c1ba1b0b7a64e3ccc802774f27c08
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-09 16:25:37 +0530

    username added a in userSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     history: [
         {
             type: mongoose.Schema.Types.ObjectId,
