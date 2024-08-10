c4ac9b3dbb06580a3bb64973a7ecf29c1b7624a1
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-10 22:02:33 +0530

    changed the cors origin again

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000
 const app = express();
 app.use(cors(
             {
-                        origin: process.env.CLIENT_URL,
+                        origin: "*",
             }
 ));
 app.use(bodyParser.json());
